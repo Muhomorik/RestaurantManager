22f020b728a8dc3faed91694ffa8249c6da3b5a2
Dmitri Peredera <dmitri.peredera@gmail.com>
2015-09-30 23:21:19 +0300

    Order page buttons and AppBar.

--- RestaurantManager.UniversalWindows/OrderPage.xaml.cs
+++ RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -26,5 +26,41 @@ namespace RestaurantManager.UniversalWindows
         {
             this.InitializeComponent();
         }
+
+
+        // https://msdn.microsoft.com/sv-se/library/windows/apps/windows.ui.xaml.controls.appbar
+        // Important  You should use the AppBar only when you are upgrading a 
+        // Universal Windows 8 app that uses the AppBar, and need to minimize changes. 
+        // For new apps in Windows 10, we recommend using the CommandBar control instead.
+
+        /// <summary>
+        /// **Add To Order** button pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAddToOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// **Submit Order** button pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSubmitOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Home clicked on OrderPage.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void appBarHomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
     }
 }
